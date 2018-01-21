a6f3b9d87ded80abbc98c451c5eb85736dccf220
kramduckner <buckner.a.mark@gmail.com>
2018-01-21 11:41:27 -0700

    added some graphs to reports

--- src/Report/Report.js
+++ src/Report/Report.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {XAxis, YAxis, Tooltip, Legend, Bar, BarChart, CartesianGrid} from 'recharts';
+import {XAxis, YAxis, Tooltip, Legend, Bar, BarChart, CartesianGrid, LineChart, Line} from 'recharts';
 import moment from 'moment';
 
   //water usage by week
@@ -9,42 +9,64 @@ import moment from 'moment';
   //% reclaimed water
   //water usage by zone?
 
-  //this is good i think
+  const dumbData = [
+    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+  ];
 
-  //what about today's activity?
-      const data = [{
-      name: "2018-01-15T18:00:00+00:00",
+
+    const data = [{
+      startTime: moment("2018-01-15T18:00:00+00:00").format("l"),
       duration:300000,
       zones:[3, 5]
     },
     {
       duration:300000,
-      name: "2018-01-17T18:00:00+00:00",
+      startTime: moment("2018-01-17T18:00:00+00:00").format("l"),
       zones:[3, 5]
     },
     {
       duration:300000,
-      startTime: "2018-01-19T18:00:00+00:00",
+      startTime: moment("2018-01-19T18:00:00+00:00").format("l"),
       zones:[3, 5]
     },
     {
       duration:300000,
-      startTime: "2018-01-21T18:00:00+00:00",
+      startTime: moment("2018-01-21T18:00:00+00:00").format("l"),
       zones:[3, 5]
     },
     {
       duration:300000,
-      startTime: "2018-01-23T18:00:00+00:00",
+      startTime: moment("2018-01-23T18:00:00+00:00").format("l"),
       zones:[3, 5]
     },
     {
       duration:300000,
-      startTime: "2018-01-25T18:00:24+00:00",
+      startTime: moment("2018-01-25T18:00:24+00:00").format("l"),
       zones:[3, 5]
     }]
 
+    var lastStartTime;
+    data.forEach((w, i)=>{
+      if (i > 0){
+       var duration = moment(w.startTime).diff(moment(lastStartTime), "hours");
+       if (duration > 24){
+        data.splice(i, 0, {
+          duration:0,
+          startTime:moment(w.startTime).subtract(1,"d").format("l")
+          });
+        }
+      }
+      lastStartTime = w.startTime;
+    });
+
 export default class Report extends Component {
-  
+
   constructor(props){
     super(props);
     this.state = {
@@ -66,19 +88,39 @@ export default class Report extends Component {
     } else {
       return(
           <div>
-            <BarChart width={600} height={300} data={data}
+            <h3> Water Usage by Week </h3>
+            <BarChart width={1000} height={300} data={data}
               margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-              <XAxis dataKey="name"/>
-              <YAxis/>
+              <XAxis dataKey="startTime"/>
+              <YAxis height={400000}/>
               <CartesianGrid strokeDasharray="3 3"/>
               <Tooltip/>
               <Legend />
-              {/* <Bar dataKey="pv" fill="#8884d8" /> */}
               <Bar dataKey="duration" fill="#82ca9d" />
+            </BarChart>
+            <h3> Water Usage by Zone </h3>
+            <LineChart width={1000} height={250} data={dumbData}
+              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+            <h3> Water Usage by Month </h3>
+            <BarChart width={1000} height={300} data={data}
+              margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+              <XAxis dataKey="startTime"/>
+              <YAxis height={400000}/>
+              <CartesianGrid strokeDasharray="3 3"/>
+              <Tooltip/>
+              <Legend />
+              <Bar dataKey="duration" fill="green" />
             </BarChart>
           </div>
     )
   }
   }
 }
-
